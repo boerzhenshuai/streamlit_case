3d0fae0d9b38bf5daf61ee90b9ab72ea14fc4837
DESKTOP-7IAJ73P\86176 <2319612664@qq.com>
2025-04-01 11:06:27 +0800

    put in 2025/4/1 10:33

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@
 # @File : streamlit_app.py
 # @Software: PyCharm
 import streamlit as st
-import openai
+from openai import OpenAI
 st.title("Chatbot")
 if "messages" not in st.session_state:
     st.session_state["messages"]=[{"role":"assistant","content":"How can I help you?"}]
@@ -13,11 +13,10 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    openai.api_key="sk-c4b8c501c53843909356cb43e3e42797"
-    openai.base_url="https://api.deepseek.com"
+    client = OpenAI(api_key="sk-c4b8c501c53843909356cb43e3e42797", base_url="https://api.deepseek.com")
     st.session_state.messages.append({"role":"user","content":prompt})
     st.chat_message("user").write(prompt)
-    response=openai.ChatCompletion.create(model="deepseek-chat",
+    response=client.chat.completions.create(model="deepseek-chat",
         messages=st.session_state.messages)
     msg=response.choices[0].message
     st.session_state.messages.append(msg)
