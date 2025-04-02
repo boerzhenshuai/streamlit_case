80b0f627715c04af4752dabb6f933522f873a268
DESKTOP-7IAJ73P\86176 <2319612664@qq.com>
2025-04-02 09:40:54 +0800

    put in 2025/4/1 10:33

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,6 @@ if prompt := st.chat_input():
         stream=False
                                             )
     msg=response.choices[0].message
+    print(msg)
     st.session_state.messages.append(msg)
     st.chat_message("assistant").write(msg.content)
